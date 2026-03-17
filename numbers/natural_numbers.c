7860604528266b5fede9687b673efcbede7406db
THARUN D S <tharunds223@gmail.com>
2026-03-17 22:09:51 +0530

    Add c programs for number problem solving

---
+++ numbers/natural_numbers.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int x;
+    printf("enter no\n");
+    scanf("%d",&x);
+    if(x>0)
+    printf("it is natural");
+else 
+printf("it is not natural");
+return 0;
+}
